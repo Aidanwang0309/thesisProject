370f8a6188bfbc34a7f96a956fb0e844b4ff3c13
Aidanwang0309 <aidanwang0309@gmail.com>
2019-02-22 03:40:20 -0500

    update client-side cookie session

--- server.js
+++ server.js
@@ -1,25 +1,39 @@
-const express = require('express');
-const keys = require('./config/keys')
-const mongoose = require('mongoose');
-const expressGraphQL = require('express-graphql');
-const schema = require('./schema/schema');
-const authRoutes = require('./routes/authRoutes');
-require('./models/User');
-require('./services/passport');
+const express = require("express");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
+const mongoose = require("mongoose");
+const expressGraphQL = require("express-graphql");
+const schema = require("./schema/schema");
+const authRoutes = require("./routes/authRoutes");
+require("./models/User");
+require("./services/passport");
 
-mongoose.connect(keys.mongoURI, {useNewUrlParser: true });
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
 const app = express();
+
+// setup client-side cookie session
+app.use(
+	cookieSession({
+		maxAge: 30 * 24 * 60 * 60 * 1000,
+		keys: [keys.cookieKey]
+	})
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 // setup routes of authentication
 authRoutes(app);
 
+app.use(
+	"/graphql",
+	expressGraphQL({
+		schema,
+		graphiql: true
+	})
+);
 
-app.use('/graphql', expressGraphQL({
-    schema,
-    graphiql: true
-}));
-
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Listening ${PORT}`);
+	console.log(`Listening ${PORT}`);
 });
--- services/passport.js
+++ services/passport.js
@@ -1,28 +1,38 @@
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('../config/keys');
-const mongoose = require('mongoose');
-const User = mongoose.model('users');
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("../config/keys");
+const mongoose = require("mongoose");
+const User = mongoose.model("users");
 
 // The value passed to `done` here is stored on the session.
 // We save the full user object in the session.
 passport.serializeUser((user, done) => {
-    done(null, user.id);
-  });
-  
+	done(null, user.id);
+});
+
 // The value returned from `serializeUser` is passed in from the session here,
 // to get the user. We save the full user object in the session.
-passport.deserializeUser((user, done) => {
-    done(null, user.id);
-  });
+passport.deserializeUser((id, done) => {
+	User.findById(id).then(user => {
+		done(null, user);
+	});
+});
 
-passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL:'/auth/google/callback'
-}, (accessToken, refreshToken, profile, done) => {
-    User.findOne({ googleID: profile.id})
-    .then((existingUser) => {
-        existingUser ? done(null, existingUser) : new User({ googleId: profile.id }).save().then(user => done(null, user))
-    })
-}));
+passport.use(
+	new GoogleStrategy(
+		{
+			clientID: keys.googleClientID,
+			clientSecret: keys.googleClientSecret,
+			callbackURL: "/auth/google/callback"
+		},
+		(accessToken, refreshToken, profile, done) => {
+			User.findOne({ googleID: profile.id }).then(existingUser => {
+				existingUser
+					? done(null, existingUser)
+					: new User({ googleId: profile.id })
+							.save()
+							.then(user => done(null, user));
+			});
+		}
+	)
+);
